320faf9c11fffba3ec632c5d62206fb31a6f4ee2
ZLoo <Kozlov.s.v.1992@gmail.com>
2023-08-31 09:35:51 +0300

    Добавьте файлы проекта.

---
+++ HW4SW/Configure.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Ports;
+
+namespace HW4SW;
+
+public class Configure
+{
+    public string ComPortName { get; set; } = "COM2";
+    public int BaudRate { get; set; } = 9600;
+    public int DataBits { get; set; } = 8;
+    public Parity Parity { get; set; } = Parity.None;
+    public StopBits StopBits { get; set; } = StopBits.One;
+    public string NatsUrl { get; set; } = "nats://localhost:4222";
+    public string Tittle { get; set; } = "bytes";
+}
---
+++ HW4SW/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.IO.Ports;
+using System.Text;
+using HW4SW;
+using Microsoft.Extensions.Configuration;
+using NATS.Client;
+
+var builder = new ConfigurationBuilder()
+.SetBasePath(Directory.GetCurrentDirectory())
+.AddJsonFile("appsettings.json", optional: false);
+
+IConfiguration config = builder.Build();
+Configure configure = config.GetSection("Configure").Get<Configure>() ?? new();
+
+if (args.Length is 1 && args[0] == "nats")
+{
+    Options opts = ConnectionFactory.GetDefaultOptions();
+    opts.Url = configure.NatsUrl;
+    using IConnection connection = new ConnectionFactory().CreateConnection(opts);
+    ISyncSubscription subSync = connection.SubscribeSync(configure.Tittle);
+    while (true)
+    {
+        try
+        {
+            Msg m = subSync.NextMessage(1000);
+            string text = Encoding.UTF8.GetString(m.Data);
+            Console.WriteLine($"Received: '{text}' from '{m.Subject}'");
+            m = subSync.NextMessage(100);
+        }
+
+        catch (NATSTimeoutException)
+        {
+            Console.WriteLine("No new messages");
+        }
+        Console.WriteLine("Wait 1 sec");
+        await Task.Delay(1000);
+    }
+}
+
+
+if (args.Length is 0)
+{
+    using SerialPort comPort = new
+    (
+        configure.ComPortName,
+        configure.BaudRate,
+        configure.Parity,
+        configure.DataBits,
+        configure.StopBits
+    );
+
+    try
+    {
+        comPort.Open();
+        Options opts = ConnectionFactory.GetDefaultOptions();
+        opts.Url = configure.NatsUrl;
+
+        using IConnection connection = new ConnectionFactory().CreateConnection(opts);
+        Console.WriteLine("Read data from COM port and send to NATS");
+
+        List<char> chars = new();
+        while (true)
+        {
+            int read = comPort.ReadByte();
+            if (read != -1)
+            {
+                char c = (char)read;
+                switch (c)
+                {
+                    case '\r':
+                    {
+                        if (chars.Count > 0)
+                        {
+                            string source = string.Join(string.Empty, chars);
+                            Console.WriteLine($"send message: {source}");
+                            byte[] bytes = Encoding.UTF8.GetBytes(source);
+                            connection.Publish(configure.Tittle, bytes);
+                            chars = new();
+                        }
+                        break;
+                    }
+                    case '\n': break;
+                    default: chars.Add(c); break;
+                }
+            }
+            await Task.Delay(1000);
+        }
+    }
+    catch (Exception ex) { Console.WriteLine($"Error: {ex.Message}"); }
+    finally { comPort.Close(); }
+}
